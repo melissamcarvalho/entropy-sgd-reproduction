29f6cc4da11e011f37af7c8105241d2b22a13ad4
Melissa Carvalho <mcarvalho.melissa@gmail.com>
2021-04-25 17:34:30 -0300

    add total loops to the loader,  add train_eval and execute linter

--- entropy-sgd/loader.py
+++ entropy-sgd/loader.py
@@ -1,32 +1,37 @@
+import os
+import sys
 import torch as th
-from torchvision import datasets, transforms
+import math
 import torch.utils.data
 import numpy as np
-import os, sys, pdb
+
+from torchvision import datasets
+
 
 class sampler_t:
-    def __init__(self, batch_size, x,y, train=True):
+    def __init__(self, batch_size, x, y, train=True):
         self.n = x.size(0)
-        self.x, self.y = x,y
+        self.x, self.y = x, y
         self.b = batch_size
         self.idx = th.arange(0, self.b).long()
         self.train = train
+        self.train_index = 0
         self.sidx = 0
+        self.total_loops = int(math.ceil(self.n/self.b))
 
     def __next__(self):
         if self.train:
-            self.idx.random_(0,self.n)
+            self.idx.random_(0, self.n)
         else:
             s = self.sidx
             e = min(s+self.b-1, self.n)
-            #print s,e
 
             self.idx = th.arange(s, e).long()
             self.sidx += self.b
             if self.sidx >= self.n:
                 self.sidx = 0
 
-        x,y  = th.index_select(self.x, 0, self.idx), \
+        x, y  = th.index_select(self.x, 0, self.idx), \
             th.index_select(self.y, 0, self.idx)
         return x, y
 
@@ -34,6 +39,7 @@ class sampler_t:
 
     def __iter__(self):
         return self
+
 
 def mnist(opt):
     d1, d2 = datasets.MNIST('../proc', download=True, train=True), \
@@ -45,13 +51,17 @@ def mnist(opt):
         d2.test_labels, train=False)
     return train, val, val
 
+
 def cifar10(opt):
-    loc = '../proc/'
+    loc = './proc/'
     d1 = np.load(loc+'cifar10-train.npz')
     d2 = np.load(loc+'cifar10-test.npz')
 
     train = sampler_t(opt['b'], th.from_numpy(d1['data']),
                      th.from_numpy(d1['labels']))
-    val = sampler_t(opt['b'], th.from_numpy(d2['data']),
+    train_eval = sampler_t(opt['eval_b'], th.from_numpy(d1['data']),
+                     th.from_numpy(d1['labels']))
+    val = sampler_t(opt['eval_b'], th.from_numpy(d2['data']),
                      th.from_numpy(d2['labels']), train=False)
-    return train, val, val
+
+    return train, train_eval, val
