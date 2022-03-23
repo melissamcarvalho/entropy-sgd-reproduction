72286abd12af609eab078418710e8a1186090102
Melissa Carvalho <mcarvalho.melissa@gmail.com>
2022-03-23 13:48:16 -0300

    add update on reproduction experiment

--- entropy-sgd/final-experiments.sh
+++ entropy-sgd/final-experiments.sh
@@ -7,46 +7,50 @@
 # # # As the scoping scheme, we set the initial value of the scope to γ0 = 0.03 which increases 
 # # # by a factor of 1.001 after each parameter update.
 
-# Date: 05/09/2021
+# Date: 23/03/2022
 # Reproduction experiments trying the stopping criteria
 python train.py \
         -B 10 \
-        -exp-tag entropy_001 \
+        -exp-tag entropy_reproduction \
         -wandb-mode online \
         -m allcnn \
         -b 100 \
-        -eval-b 1000 \
-        --lr 1 \
-        --l2 0 \
+        -eval-b 100 \
+        -lr 1 \
+        -l2 0 \
         -L 20 \
         -g 0 \
         -s 51 \
         -epoch-step 1 \
         -batch-step 100 \
-        --lr-step 4 \
-        --lr-decay 0.2 \
-        --gamma 0.03 \
-        --scoping 0.001 \
-        --noise 0.0001 \
-        --nesterov \
-        --apply-scoping \
-        --min-loss 0.1
+        -lr-step 4 \
+        -lr-decay 0.2 \
+        -gamma 0.03 \
+        -scoping 0.001 \
+        -noise 0.0001 \
+        -nesterov \
+        -momentum 0.9 \
+        -apply-scoping \
+        -min-loss 0.1 \
+        -calculate
     
 python train.py \
         -B 200 \
-        -exp-tag sgd_001 \
+        -exp-tag sgd_reproduction \
         -wandb-mode online \
         -m allcnn \
         -b 100 \
-        -eval-b 1000 \
-        --lr 0.1 \
-        --l2 0 \
+        -eval-b 100 \
+        -lr 0.1 \
+        -l2 0 \
         -L 0 \
         -g 0 \
         -s 51 \
         -epoch-step 20 \
         -batch-step 100 \
-        --lr-step 60 \
-        --lr-decay 0.2 \
-        --nesterov \
-        --min-loss 0.1
+        -lr-step 60 \
+        -lr-decay 0.2 \
+        -nesterov \
+        -momentum 0.9 \
+        -min-loss 0.1 \
+        -calculate
