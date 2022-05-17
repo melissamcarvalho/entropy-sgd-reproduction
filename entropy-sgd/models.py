2ecb20513649b4afd58c94621b1f74c396ea02bf
Melissa Carvalho <mcarvalho.melissa@gmail.com>
2022-05-17 11:57:42 -0300

    remove unused classes: mnist models and ExperimentBaseModel

--- entropy-sgd/models.py
+++ entropy-sgd/models.py
@@ -1,8 +1,6 @@
 import torch.nn as nn
 from torch import Tensor
 
-from experiment_config import DatasetType
-
 
 class View(nn.Module):
     def __init__(self, o):
@@ -16,65 +14,6 @@ class View(nn.Module):
 def num_parameters(model):
     return sum([w.numel() for w in model.parameters()])
 
-
-# TODO: Remove MNIST code
-class mnistfc(nn.Module):
-    def __init__(self, opt):
-        super(mnistfc, self).__init__()
-        self.name = 'mnsitfc'
-
-        c = 1024
-        opt['d'] = 0.5
-
-        self.m = nn.Sequential(
-            View(784),
-            nn.Dropout(0.2),
-            nn.Linear(784, c),
-            nn.BatchNorm1d(c),
-            nn.ReLU(True),
-            nn.Dropout(opt['d']),
-            nn.Linear(c, c),
-            nn.BatchNorm1d(c),
-            nn.ReLU(True),
-            nn.Dropout(opt['d']),
-            nn.Linear(c, 10))
-
-        s = '[%s] Num parameters: %d' % (self.name, num_parameters(self.m))
-        print(s)
-
-    def forward(self, x):
-        return self.m(x)
-
-
-class mnistconv(nn.Module):
-    def __init__(self, opt):
-        super(mnistconv, self).__init__()
-        self.name = 'mnistconv'
-        opt['d'] = 0.5
-
-        def convbn(ci, co, ksz, psz, p):
-            return nn.Sequential(
-                nn.Conv2d(ci, co, ksz),
-                nn.BatchNorm2d(co),
-                nn.ReLU(True),
-                nn.MaxPool2d(psz, stride=psz),
-                nn.Dropout(p))
-
-        self.m = nn.Sequential(
-            convbn(1, 20, 5, 3, opt['d']),
-            convbn(20, 50, 5, 2, opt['d']),
-            View(50 * 2 * 2),
-            nn.Linear(50 * 2 * 2, 500),
-            nn.BatchNorm1d(500),
-            nn.ReLU(True),
-            nn.Dropout(opt['d']),
-            nn.Linear(500, 10))
-
-        s = '[%s] Num parameters: %d' % (self.name, num_parameters(self.m))
-        print(s)
-
-    def forward(self, x):
-        return self.m(x)
 
 
 class allcnn(nn.Module):
@@ -111,14 +50,3 @@ class allcnn(nn.Module):
 
     def forward(self, x):
         return self.m(x)
-
-
-# Currently being used for typing, we may remove it
-# TODO: Remove it from typing
-class ExperimentBaseModel(nn.Module):
-    def __init__(self, dataset_type: DatasetType):
-        super().__init__()
-        self.dataset_type = dataset_type
-
-    def forward(self, x) -> Tensor:
-        raise NotImplementedError
